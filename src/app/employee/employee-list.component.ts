7f73e6d9831d7571e9efd3b64641990dc83b2533
Chikkala <sivarjauna.chikkala@accenture.com>
2018-08-07 19:03:06 +0530

    Navbar and bootstrap4 added

--- src/app/employee/employee-list.component.ts
+++ src/app/employee/employee-list.component.ts
@@ -1,12 +1,19 @@
 import { Component, OnInit } from '@angular/core';
+import { Employee } from './employee';
 
 @Component({
   selector: 'myapp-employee-list',
   templateUrl: './employee-list.component.html',
   styleUrls: ['./employee-list.component.css']
 })
-export class EmployeeListComponent implements OnInit {
+export class EmployeeListComponent implements OnInit 
+{
 
+employees:Employee[]=[
+  {Id:11, Name:'Sivarjun', EmployeeId:1121, Street1:'East',Street2:'Opp. Dominos',zip:523272,City:'Ongole',State:'A.P.'},
+  {Id:12, Name:'Gopi', EmployeeId:8976, Street1:'West',Street2:'Behind Govt School',zip:523001,City:'Ongole',State:'A.P.'},
+  {Id:13, Name:'Raju', EmployeeId:4545, Street1:'North',Street2:'Near Well',zip:526274,City:'Ongole',State:'A.P.'}
+]
   constructor() { }
 
   ngOnInit() {
---
+++ src/app/employee/employee.ts
@@ -0,0 +1,10 @@
+export class Employee {
+         Id:number;
+         Name:string;
+         EmployeeId:number;
+         Street1:string;
+         Street2:string;
+         City:string;
+         State:string;
+         zip:number;
+}
