786005c23331a6a810333c1f01d7a876f8cc1295
Chikkala <sivarjauna.chikkala@accenture.com>
2018-08-07 12:58:17 +0530

    added home employee and page not found component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+import {HomeComponent} from './home/home.component';
+import {EmployeeListComponent} from './employee/employee-list.component';
+import {EmployeeDetailComponent} from './employee/employee-detail/employee-detail.component';
+import {AddEmployeeComponent} from './employee/add-employee/add-employee.component';
+import {PageNotFoundComponent} from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'home', redirectTo: ''},
+  {path:'employee', component:EmployeeListComponent},
+  {path:'employee/:id', component:AddEmployeeComponent},
+  {path:'employee/details/:id', component:EmployeeDetailComponent},
+  {path:'**', component:PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,21 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { EmployeeListComponent } from './employee/employee-list.component';
+import { EmployeeDetailComponent } from './employee/employee-detail/employee-detail.component';
+import { AddEmployeeComponent } from './employee/add-employee/add-employee.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    EmployeeListComponent,
+    EmployeeDetailComponent,
+    AddEmployeeComponent,
+    PageNotFoundComponent
   ],
   imports: [
     BrowserModule,
